d933be7feaf42c7d3a9ede1f5166c8cb2114475b
Jyhess <jyhess@gmail.com>
2025-05-07 18:19:34 +0200

    Fix android build with redux

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,6 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 
 // Configuration des extensions
-config.resolver.sourceExts = ['jsx', 'js', 'ts', 'tsx', 'json'];
+config.resolver.sourceExts = ['jsx', 'js', 'ts', 'tsx', 'cjs', 'json'];
 
 module.exports = config; 
