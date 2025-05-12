6c3df2bdb024ea3408e8bc992a973920a3197e57
Jyhess <jyhess@gmail.com>
2025-05-12 14:04:47 +0200

    Ajout de la couleur aux alertes

--- src/types/alerts/index.ts
+++ src/types/alerts/index.ts
@@ -1,3 +1,3 @@
-export { Alert, AlertType } from './alert.types';
+export { Alert, AlertType, ALERT_COLORS, AlertColor } from './alert.types';
 export { isAlertActive, isAlertValid, hasAlertTimeOffset, shouldAlertTrigger, getAlertTimeText, getAlertTimePrefix, getAlertTitle, sortAlerts } from './alert.utils';
 export { DEFAULT_ALERTS } from './alert.default';
