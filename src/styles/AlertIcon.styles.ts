660037fa89b096298c07310dac2c5888df2dded1
Jyhess <jyhess@gmail.com>
2025-05-13 00:58:21 +0200

    Improve TimeIcon presentation

--- src/styles/AlertIcon.styles.ts
+++ src/styles/AlertIcon.styles.ts
@@ -1,51 +1,42 @@
-import { StyleSheet } from 'react-native';
-import { theme } from '../theme';
+import { StyleSheet } from "react-native";
+import { theme } from "../theme";
 
 export const styles = StyleSheet.create({
   alertItemContainer: {
+    flexDirection: 'row',
     alignItems: 'center',
-    gap: theme.spacing.small,
     backgroundColor: theme.colors.background.tertiary,
-    padding: theme.spacing.medium,
+    padding: theme.spacing.small,
     borderRadius: theme.borders.radius.large,
-    minWidth: 40,
+    gap: theme.spacing.xs,
+    width: theme.layout.minWidth,
   },
-  alertIconContainer: {
-    position: 'relative',
-    width: '100%',
-    alignItems: 'center',
+  timeContainer: {
+    minWidth: 50,
   },
-  alertIcon: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: theme.spacing.xs,
-  },
-  alertIconActive: {
-    opacity: 1,
-  },
-  alertIconDisabled: {
-    opacity: theme.effects.opacity.disabled,
+  nameContainer: {
+    flex: 1,
+    marginLeft: theme.spacing.xs,
   },
   alertTime: {
     fontSize: theme.typography.fontSize.small,
     fontWeight: theme.typography.fontWeight.medium,
-    color: theme.colors.gray.light,
-    marginTop: theme.spacing.xs,
     textAlign: 'center',
     letterSpacing: 0.5,
   },
+  alertName: {
+    fontSize: theme.typography.fontSize.medium,
+    fontWeight: theme.typography.fontWeight.medium,
+  },
+  colorDot: {
+    width: theme.layout.smallIconSize,
+    height: theme.layout.smallIconSize,
+    borderRadius: theme.borders.radius.round,
+  },
+  soundIcon: {
+    marginHorizontal: theme.spacing.xs,
+  },
   sliderContainer: {
     width: 50,
-    alignItems: 'center',
-    marginTop: theme.spacing.xs,
-  },
-  effectIconsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginTop: theme.spacing.xs,
-    gap: theme.spacing.xs,
-  },
-  effectIcon: {
-    opacity: theme.effects.opacity.disabled,
   },
 });
